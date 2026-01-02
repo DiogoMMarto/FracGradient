cb69839b3e458ed36a87236416438bf469d069e5
Diogo Marto <diogo.marto@ua.pt>
2026-01-02 04:17:23 +0000

    NN files

--- src/run_all.py
+++ src/run_all.py
@@ -18,7 +18,11 @@ def main():
     # run all main*.py files in the RUN_DIR
     run_dir = pathlib.Path(RUN_DIR)
     main_files = list(run_dir.glob("main_*.py"))
-    for main_file in main_files:
+    
+    tf_files = list(run_dir.glob("main_cifar*.py")) + list(run_dir.glob("main_*cnn*.py"))
+    other_files = [f for f in main_files if f not in tf_files]
+    
+    for main_file in other_files:
         print(f"Running {main_file.name}...")
         subprocess.run([PYTHON_VERSION, str(main_file)], check=True)
     print("All main files executed successfully.")
